f47b460d6895c104135282f96b83b34dd813a71b
Aatif Ahmad <b22ai002@iitj.ac.in>
2024-08-17 00:34:46 +0530

    commit four

--- Approach_5_Counter/lru.cpp
+++ Approach_5_Counter/lru.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<vector>
+#include<chrono>
 using namespace std;
 
 /*
 
 Data Structure(s) Used : vector<pair<pair<int,char>,int>>
 
-Verdict : 20/22 test cases passed on LC [TLE]
+Verdict : 21/23 test cases passed on LC [TLE]
 
 */
 
 class LRUCache {
     int cap; // capacity
-    vector<pair<pair<int,char>,int>> v; // vector to store key-value pairs and counters
+    vector<pair<pair<int,int>,int>> v; // vector to store key-value pairs and counters
     int curr; // current size
 
 public:
@@ -22,11 +23,11 @@ public:
         curr = 0;
     }
     
-    char get(int key) {
+    int get(int key) {
         // check if the key is present or not
         bool found = false;
         int keycounter = 0;
-        char ch;
+        int ch;
         for(auto &p : v){
             if(p.first.first == key){
                 found = true;
@@ -38,7 +39,7 @@ public:
         }
         if(!found){
             // if not present, then return '#'
-            return '#';
+            return -1;
         }
         else{
             /*
@@ -54,7 +55,7 @@ public:
         }
     }
     
-    void put(int key, char value) {
+    void put(int key, int value) {
         /*
         first check, if there is some space left on the cache
         */
@@ -117,8 +118,8 @@ public:
             }
             else if(!found){
                 curr++;
-                pair<int,char> p = make_pair(key,value);
-                pair<pair<int,char>,int> temp = make_pair(p,curr);
+                pair<int,int> p = make_pair(key,value);
+                pair<pair<int,int>,int> temp = make_pair(p,curr);
                 v.push_back(temp);
             }
         }
@@ -127,12 +128,109 @@ public:
 
 int main(){
     
-    LRUCache lruc(2);
-    lruc.put(1,'A');
-    lruc.put(2,'B');
-    cout << lruc.get(1) << endl;
-    lruc.put(3,'C');
-    cout << lruc.get(2) << endl;
+    LRUCache lruc(10); // Initialize the LRU cache with a capacity of 10
+
+    auto start = chrono::high_resolution_clock::now();
+
+    vector<int> outputs;
+
+    for(int i = 0;i < 100000;i++){
+        lruc.put(10, 13);
+        lruc.put(3, 17);
+        lruc.put(6, 11);
+        lruc.put(10, 5);
+        lruc.put(9, 10);
+        outputs.push_back(lruc.get(13));
+        lruc.put(2, 19);
+        outputs.push_back(lruc.get(2));
+        outputs.push_back(lruc.get(3));
+        lruc.put(5, 25);
+        outputs.push_back(lruc.get(8));
+        lruc.put(9, 22);
+        lruc.put(5, 5);
+        lruc.put(1, 30);
+        outputs.push_back(lruc.get(11));
+        lruc.put(9, 12);
+        outputs.push_back(lruc.get(7));
+        outputs.push_back(lruc.get(5));
+        outputs.push_back(lruc.get(8));
+        outputs.push_back(lruc.get(9));
+        lruc.put(4, 30);
+        outputs.push_back(lruc.get(9));
+        outputs.push_back(lruc.get(10));
+        lruc.put(6, 14);
+        outputs.push_back(lruc.get(3));
+        lruc.put(10, 11);
+        outputs.push_back(lruc.get(8));
+        lruc.put(2, 14);
+        outputs.push_back(lruc.get(1));
+        outputs.push_back(lruc.get(5));
+        outputs.push_back(lruc.get(4));
+        lruc.put(11, 4);
+        lruc.put(12, 24);
+        lruc.put(5, 18);
+        outputs.push_back(lruc.get(13));
+        outputs.push_back(lruc.get(7));
+        outputs.push_back(lruc.get(8));
+        outputs.push_back(lruc.get(12));
+        lruc.put(3, 27);
+        lruc.put(2, 12);
+        outputs.push_back(lruc.get(5));
+        lruc.put(2, 9);
+        outputs.push_back(lruc.get(13));
+        lruc.put(8, 18);
+        lruc.put(1, 7);
+        lruc.put(6, 30);
+        outputs.push_back(lruc.get(9));
+        lruc.put(8, 21);
+        outputs.push_back(lruc.get(5));
+        lruc.put(6, 30);
+        outputs.push_back(lruc.get(1));
+        outputs.push_back(lruc.get(10));
+        lruc.put(4, 15);
+        outputs.push_back(lruc.get(7));
+        lruc.put(11, 26);
+        outputs.push_back(lruc.get(8));
+        outputs.push_back(lruc.get(9));
+        lruc.put(5, 29);
+        outputs.push_back(lruc.get(3));
+        lruc.put(11, 30);
+        outputs.push_back(lruc.get(12));
+        lruc.put(4, 29);
+        outputs.push_back(lruc.get(3));
+        outputs.push_back(lruc.get(9));
+        lruc.put(6, 30);
+        outputs.push_back(lruc.get(3));
+        lruc.put(10, 28);
+        outputs.push_back(lruc.get(1));
+        lruc.put(11, 13);
+        outputs.push_back(lruc.get(3));
+        outputs.push_back(lruc.get(3));
+        lruc.put(12, 24);
+        outputs.push_back(lruc.get(9));
+        outputs.push_back(lruc.get(6));
+        outputs.push_back(lruc.get(3));
+        lruc.put(13, 17);
+        outputs.push_back(lruc.get(2));
+        lruc.put(11, 15);
+        lruc.put(12, 24);
+        outputs.push_back(lruc.get(9));
+        outputs.push_back(lruc.get(2));
+        outputs.push_back(lruc.get(15));
+        outputs.push_back(lruc.get(11));
+        lruc.put(4, 29);
+        outputs.push_back(lruc.get(6));
+        outputs.push_back(lruc.get(19));
+        outputs.push_back(lruc.get(4));
+        lruc.put(13, 28);
+        lruc.put(11, 26);
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> duration = end - start;
+
+    cout << "Time taken: " << duration.count() << " seconds" << endl;
 
     return 0;
 }
